80878377f13a4170996f9493ea65004e8712b9c6
mayur4895 <kssmsslss330@gmail.com>
2024-11-30 22:10:30 +0530

    " eslint rule disable"

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+// next.config.js
+module.exports = {
+    reactStrictMode: true,
+    eslint: {
+      ignoreDuringBuilds: true,
+      },
+    
+    images: {
+      domains: ['portfolio-image-store.s3.ap-south-1.amazonaws.com']  },
+    // ...other config settings
+  };
+  
